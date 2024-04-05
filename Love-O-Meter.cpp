e264f1554ba33a2589f2ec972bc1b3ff0a9e352a
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-04-05 10:48:04 -0600

    Update Love-O-Meter.cpp

--- Love-O-Meter.cpp
+++ Love-O-Meter.cpp
@@ -1,4 +1,4 @@
-const int sensorPin = AO;
+const int sensorPin = A0;
 const float baselineTemp = 20.0;
 void setup() {
   Serial.begin(9600);
@@ -15,9 +15,9 @@ void loop(){
   float voltage = (sensorVal/1024.0) * 5.0;
   Serial.print(", Volts: ");
   Serial.print(voltage);
-  Serial.print(", degrees C: ")
+  Serial.print(", degrees C: ");
 
-  float temperature = (voltage - .5 * 100;
+  float temperature = (voltage - .5) * 100;
   Serial.println(temperature);
   if(temperature < baselineTemp+2){
     digitalWrite(2, LOW);
@@ -34,7 +34,7 @@ void loop(){
   else if(temperature >= baselineTemp+6){
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH); }
-}
+    digitalWrite(4, HIGH); 
+  }
 delay(1);
 }
